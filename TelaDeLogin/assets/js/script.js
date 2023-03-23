ae144635d99021f6c31f16d93feb3e1ad6aa4627
Musta-eideh <mustafaeideh100@outlook.com>
2023-03-23 14:38:26 -0300

    Tela login completo

--- TelaDeLogin/assets/js/script.js
+++ TelaDeLogin/assets/js/script.js
@@ -2,9 +2,6 @@ function togglePassword() {
     document.querySelectorAll(".eye").forEach(eye => 
         eye.classList.toggle("hide"))
 
-    if(senha.getAttribute('type') == 'password') {
-        senha.setAttribute('type', 'text');
-    }else{
-        senha.setAttribute('type', 'password');
-    }
+    const type = senha.getAttribute("type")  == "password" ? 'text' : 'password'
+    senha.setAttribute("type", type)
 }
